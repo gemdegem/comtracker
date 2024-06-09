228648d65a96eb44635b97ca4c1800da7b286eed
mbacaa <mbacaa@icloud.com>
2024-06-10 01:17:23 +0200

    feat: added commune ai ref

--- components/SearchPanel.tsx
+++ components/SearchPanel.tsx
@@ -3,6 +3,7 @@ import useCoinPaths from '@/hooks/useCoinPaths'
 import { query } from '@/lib/coinpath-queries'
 import { SearchForm } from './SearchForm'
 import { SearchObject } from '@/lib/types'
+import Link from 'next/link'
 
 interface SearchPanelProps {
 	setSearchData: (data: any) => void
@@ -30,13 +31,34 @@ export default function SearchPanel({ setSearchData }: SearchPanelProps) {
 	}
 
 	return (
-		<div className="p-5">
-			<SearchForm
-				formValues={formValues}
-				setFormValues={setFormValues}
-				findConnections={findConnections}
-				loading={loading}
-			/>
+		<div className="w-full h-full flex flex-col justify-between p-5">
+			<div>
+				<div>
+					<Link
+						href="/"
+						className="mt-5 w-full flex justify-center text-center text-4xl md:text-5xl font-thin text-black dark:text-white"
+					>
+						COMTRACKER
+					</Link>
+					<div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+				</div>
+				<SearchForm
+					formValues={formValues}
+					setFormValues={setFormValues}
+					findConnections={findConnections}
+					loading={loading}
+				/>
+			</div>
+			<div className="mx-auto text-xs">
+				Powered by{' '}
+				<Link
+					href="https://communeai.org"
+					target="_blank"
+					className="hover:text-green-500 transition-colors ease-in-out duration-300"
+				>
+					Commune AI
+				</Link>
+			</div>
 		</div>
 	)
 }
